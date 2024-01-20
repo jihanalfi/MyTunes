d98c1535b0e2f66e3b75945ef411e23c901cccb7
jihanalfi <jihanalfi67@gmail.com>
2024-01-20 11:31:56 +0700

    update: player view and functionality

--- MyTunes/Features/Common/Presenters/Views/PlayerView.swift
+++ MyTunes/Features/Common/Presenters/Views/PlayerView.swift
@@ -9,17 +9,31 @@ import SwiftUI
 
 struct PlayerView: View {
     @ObservedObject var viewModel: PlayerViewModel
-//    var song: Song
+    var song: Song
 
     var body: some View {
         HStack{
             Button(action: {viewModel.playBefore()}, label: {
-                /*@START_MENU_TOKEN@*/Text("Button")/*@END_MENU_TOKEN@*/
+                Image(systemName: "backward.end.fill")
             })
+            .buttonStyle(PlainButtonStyle())
+            
+            Button(action: {viewModel.playPauseSong(song: song)
+            }) {
+                Image(systemName: viewModel.isPlaying ? "pause.circle" : "play.circle")
+                    .resizable()
+                    .frame(width: 50, height: 50)
+            }
+            
+            Button(action: {viewModel.playAfter()}, label: {
+                Image(systemName: "forward.end.fill")
+            })
+            .buttonStyle(PlainButtonStyle())
+
         }
     }
 }
 
 #Preview {
-    PlayerView(viewModel: PlayerViewModel())
+    PlayerView(viewModel: PlayerViewModel(), song: Song(title: "Dancing Queen", artist: "ABBA", album: "Arrival", pathFile: "ABBA - Dancing Queen", pathImage: "ABBA"))
 }
