fa43ede372d3fff7f4a680a04aea62233a93da45
jihanalfi <jihanalfi67@gmail.com>
2024-01-20 11:56:49 +0700

    update: sync media player functionality and interaction

--- MyTunes/Features/Common/Presenters/Views/PageView.swift
+++ MyTunes/Features/Common/Presenters/Views/PageView.swift
@@ -11,24 +11,38 @@ struct PageView: View {
     @ObservedObject var viewModel = PlayerViewModel()
     @State var searchText: String = ""
     @State var songList = SongRepositorySample.getSongs()
+    @State var isPlayerViewVisible = false
 
     var body: some View {
-        NavigationView{
-            List(songList, id: \.title){ song in
-                SongView(viewModel: viewModel, song: song)
+        ZStack{
+            NavigationView{
+                VStack{
+                    List(songList, id: \.title){ song in
+                        SongView(viewModel: viewModel, song: song)
+                    }
+                    
+                    
+                    if isPlayerViewVisible {
+                        PlayerView(viewModel: viewModel, song: viewModel.currentSong)
+                            .transition(.move(edge: .bottom))
+                            .animation(.easeInOut)
+                    }
+                }
+            }
+            .searchable(text: $searchText)
+            .onChange(of: searchText) { searchText in
+                if !searchText.isEmpty{
+                    songList = SongRepositorySample.getSongs().filter{ $0.title.localizedCaseInsensitiveContains(searchText) ||
+                        $0.album.localizedCaseInsensitiveContains(searchText) ||
+                        $0.artist.localizedCaseInsensitiveContains(searchText)
+                    }
+                } else {
+                    songList = SongRepositorySample.getSongs()
+                }
             }
         }
-        .searchable(text: $searchText)
-        .onChange(of: searchText) { searchText in
-            if !searchText.isEmpty{
-                songList = SongRepositorySample.getSongs().filter{ $0.title.localizedCaseInsensitiveContains(searchText) ||
-                    $0.album.localizedCaseInsensitiveContains(searchText) ||
-                    $0.artist.localizedCaseInsensitiveContains(searchText)
-                }
-            } else {
-                songList = SongRepositorySample.getSongs()
-            }
-            
+        .onChange(of: viewModel.isPlaying) { isPlaying in
+            isPlayerViewVisible = true
         }
     }
 }
--- MyTunes/Features/Common/Presenters/Views/PlayerView.swift
+++ MyTunes/Features/Common/Presenters/Views/PlayerView.swift
@@ -9,28 +9,45 @@ import SwiftUI
 
 struct PlayerView: View {
     @ObservedObject var viewModel: PlayerViewModel
-    var song: Song
+    var song: Song?
 
     var body: some View {
-        HStack{
-            Button(action: {viewModel.playBefore()}, label: {
-                Image(systemName: "backward.end.fill")
-            })
-            .buttonStyle(PlainButtonStyle())
-            
-            Button(action: {viewModel.playPauseSong(song: song)
-            }) {
-                Image(systemName: viewModel.isPlaying ? "pause.circle" : "play.circle")
-                    .resizable()
-                    .frame(width: 50, height: 50)
-            }
-            
-            Button(action: {viewModel.playAfter()}, label: {
-                Image(systemName: "forward.end.fill")
-            })
-            .buttonStyle(PlainButtonStyle())
+        VStack{
+            HStack{
+                Button(action: {viewModel.playBefore()}, label: {
+                    Image(systemName: "backward.end.fill")
+                        .resizable()
+                        .frame(width: 20, height: 20)
 
+                })
+                .buttonStyle(PlainButtonStyle())
+                
+                Button(action: {viewModel.playPauseSong(song: song!)
+                }) {
+                    Image(systemName: viewModel.isPlaying ? "pause.fill" : "play.fill")
+                        .resizable()
+                        .frame(width: 20, height: 20)
+                        .padding()
+                }
+                .buttonStyle(PlainButtonStyle())
+
+                
+                Button(action: {viewModel.playAfter()}, label: {
+                    Image(systemName: "forward.end.fill")
+                        .resizable()
+                        .frame(width: 20, height: 20)
+
+                })
+                .buttonStyle(PlainButtonStyle())
+
+            }
+            Text("slider")
         }
+        .frame(width: UIScreen.main.bounds.width, height: 100)
+        .background(.white)
+//        .frame(width: UIScreen.main.bounds.width) // or use .edgesIgnoringSafeArea(.horizontal)
+
+//        .edgesIgnoringSafeArea(.horizontal)
     }
 }
 
